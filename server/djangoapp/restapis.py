0e45d827a23ad6b3e25a33c4cf3574db73e3821b
GenericUser8 <unwound.podium_0y@icloud.com>
2025-02-22 08:30:20 -0500

    Finished Create Django Proxy Services Of Backend APIs

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,12 +11,38 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params = ""
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
+    if kwargs:
+        for key, value in kwargs.items():
+            params += f"{key}={value}&"
+
+    request_url = f"{backend_url}{endpoint}?{params}"
+    print(f"GET from {request_url}")
+
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        print("Network exception occured")
+
 # Add code for retrieving sentiments
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        return requests.get(request_url).json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occured")
 
-# def post_review(data_dict):
 # Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
